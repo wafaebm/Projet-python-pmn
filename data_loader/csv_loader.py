920d0c6533cdb2ace1a66530fc9d156a71c6068a
wafaebm <moustachirwafae@gmail.com>
2025-12-09 20:57:15 +0100

    Added CSVLoader with basic loading functionality

---
+++ data_loader/csv_loader.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from .exceptions import LoaderError
+
+
+class CSVLoader:
+    """
+    The CSV Loader take a CSV file and returns a pandas DataFrame
+    """
+
+    def init(self, filepath: str, separator: str = ","):
+        self.filepath = filepath
+        self.separator = separator
+
+    def load(self) -> pd.DataFrame:
+        """
+        Loads a CSV file using pandas.
+        Raises LoaderError if loading fails.
+        """
+        return pd.read_csv(self.filepath, sep=self.separator)
